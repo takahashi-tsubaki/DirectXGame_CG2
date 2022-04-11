11b4d685ca7127413cf8e7018cd5d5d60512d792
LC1B_15_takahashi_tsubaki <k021g1132@g.neec.ac.jp>
2022-04-11 11:46:46 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <Windows.h>
+
+#include<tchar.h>
+
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	OutputDebugStringA("Hello DirectX!!\n");
+
+	const int window_width = 960;
+	const int window_height = 480;
+
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;//コールバック関数の指定
+	w.lpszClassName = _T("DirectXTest");//アプリケーションクラス名(適当でいいです)
+	w.hInstance = GetModuleHandle(0);//ハンドルの取得
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+
+	RegisterClassEx(&w);//アプリケーションクラス(こういうの作るからよろしくってOSに予告する)
+
+	RECT wrc = { 0,0,window_width, window_height };//{0,0,window_width,window_height}
+
+	//関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,//クラス名指定
+		_T("DX12テスト"),//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,//表示x座標はOSにお任せ
+		CW_USEDEFAULT,//表示y座標はOSにお任せ
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,//親ウィンドウハンドル
+		nullptr,//メニューハンドル
+		w.hInstance,//呼び出しアプリケーションハンドル
+		nullptr);//追加パラメーター
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+		}
+
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
+	return 0;
+}
+
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) 
+{
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+			//OSに対して,アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+
